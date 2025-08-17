da002b0ab601cd7f5b7a92694cb305308c17767d
Rob Newsom <rwnewsom@gmail.com>
2025-08-17 13:34:36 -0400

    specify Chrome, add test for social icons

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -4,4 +4,12 @@ describe('Homepage', () => {
     cy.contains('Welcome');
     cy.contains('I am a back-end programmer based in Columbus, Ohio.');
   });
+
+  it('renders social icons', () => {
+    cy.visit('https://rwnewsom.github.io/');
+    cy.get('.social-icons').should('exist');
+    cy.get('.social-icons a[aria-label="Github"]').should('exist');
+    cy.get('.social-icons a[aria-label="Linkedin"]').should('exist');
+    cy.get('.social-icons a[aria-label="Goodreads"]').should('exist');
+  });
 });
