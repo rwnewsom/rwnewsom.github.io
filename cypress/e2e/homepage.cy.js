9433ca76abaebb4bf22c13ff24ba8e1a0896a77c
Rob Newsom <rwnewsom@gmail.com>
2025-08-17 13:15:11 -0400

    cypress to workflow

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,6 +1,6 @@
 describe('Homepage', () => {
   it('loads the homepage and finds intro text', () => {
-    cy.visit('http://localhost:4000');
+    cy.visit('https://rwnewsom.github.io/');
     cy.contains('Welcome');
     cy.contains('I am a back-end programmer based in Columbus, Ohio.');
   });
